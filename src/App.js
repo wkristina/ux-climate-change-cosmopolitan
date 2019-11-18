b4b5a4f79564be11fd8d03feb96622ca4435eb5e
kitty <wagnerk@uni-hildesheim.de>
2019-11-18 14:42:38 +0100

    test3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> TEST!!
+          Edit <code>src/App.js</code> help
         </p>
         <a
           className="App-link"
