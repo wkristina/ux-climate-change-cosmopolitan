c25adfc612273cc65bd202b8ec2e515468e2c1ab
wkristina <kristinawagner3@web.de>
2019-11-26 16:05:43 +0100

    filter working

--- src/components/CO2Emission.js
+++ src/components/CO2Emission.js
@@ -6,56 +6,57 @@ import CompareBoxes from './CompareBoxes.js';
 
 class CO2Emission extends Component {
     state = { 
-        emission: [],
+        emissions: [],
         tempIndexYear: 0,
         tempIndexYear2: 0,
         indexYear: 0,
-        indexYear2: 0
+        indexYear2: 0,
+        searchedYearData: [],
+        searchYear2Data: []
      }
     async componentDidMount() {
-        const url = "https://my.api.mockaroo.com/co2.json?key=8eb9e6f0 ";
+        const url = "https://my.api.mockaroo.com/co2.json?key=8eb9e6f0";
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({emission: data});
+        this.setState({emissions: data});
       }
       
-      handleInputFirst  = event => {
-        this.setState({
-          tempIndexYear: event.target.value
-        })
-        console.log(this.state.tempIndexYear);
+      handleInputFirst  = Year => {
+        let index = this.state.emissions.findIndex(
+          emission => emission.Year === parseInt(Year)
+      );
+      this.setState({
+        tempIndexYear: index
+      })
+      console.log(index);
        };
     
-       handleInputSecond  = event => {
+       handleInputSecond  = Year => {
+        let index2 = this.state.emissions.findIndex(
+          emission => emission.Year === parseInt(Year)
+      );
         this.setState({
-          tempIndexYear2: event.target.value
+          tempIndexYear2: index2
         })
-        console.log(this.state.tempIndexYear2);
+        console.log(index2);
        };
-
-       //figure this shit out, to have event value from box
-       compare = (tempIndexYear, tempIndexYear2) =>{
-        let index = this.state.Co2Emission.findIndex(
-          co2 => co2.tempIndexYear === parseInt(tempIndexYear)
-        );
-        let index2 = this.state.Co2Emission.findIndex(
-          co2 => co2.tempIndexYear2 === parseInt(tempIndexYear2)
-        );
-        if (this.state.tempIndexYear !== -1 && this.state.tempIndex2Year !== -1) 
+       
+       //why are index & index2 always -1? findIndex not working as it is supposed to
+      
+       compare = () =>{
+        if (this.state.tempIndexYearindex !== -1 && this.state.tempIndexYear2 !== -1) 
         {
           this.setState({indexYear: this.state.tempIndexYear});
           this.setState({indexYear2: this.state.tempIndexYear2});
-          
         } else {
           alert("Year could not be found.");
         }
-        console.log(this.state.indexYear);
-        console.log(this.state.indexYear2);
+        
        }
 
     render() { 
-        let co2 = this.state.emission[this.state.indexYear];
-        let co = this.state.emission[this.state.indexYear2];
+        let co2 = this.state.emissions[this.state.indexYear];
+        let co = this.state.emissions[this.state.indexYear2];
         if (co2  === undefined) return <p>There is no data.</p>;
         if (co  === undefined) return <p>There is no data.</p>;
     
@@ -82,7 +83,9 @@ class CO2Emission extends Component {
       </ResponsiveContainer>
       <CompareBoxes handleInputFirst={this.handleInputFirst}
         handleInputSecond={this.handleInputSecond}
-        compare={this.compare}/>
+        compare={this.compare}
+        year1={this.state.tempIndexYear}
+        year2={this.state.tempIndexYear2}/>
             </div>
           );
     }
--- src/components/CompareBoxes.js
+++ src/components/CompareBoxes.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 
 class CompareBoxes extends Component {
     state = {  }
+   
+    handleYear = e => {
+        
+          this.props.handleInputFirst(e.target.value);
+          console.log(e.target.value);
+      };
+    
+    handleYear2 = e =>{
+        this.props.handleInputSecond(e.target.value);
+        console.log(e.target.value);
+    };
+    
+  
     render() { 
         return ( 
             <div>
-                <div className="ui input"><input type="text" placeholder="enter year..." onChange={this.props.handleInputFirst}/></div>
-               <div className="ui input"><input type="text" placeholder="enter year..." onChange={this.props.handleInputSecond}/></div>
+                <div className="ui input"><input type="text" placeholder="enter year..." onChange={this.handleYear}/></div>
+               <div className="ui input"><input type="text" placeholder="enter year..." onChange={this.handleYear2}/></div>
                 <button className="ui button" type="button" onClick={this.props.compare}> compare </button>
             </div>
          );
